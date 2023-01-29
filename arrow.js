f804acdd262ce4de650eaa1c233aa8b3341a63d2
junhyuck park <wnsgur1855@naver.com>
2023-01-29 23:08:55 +0900

    화살표 함수에대한 이해

---
+++ arrow.js
@@ -0,0 +1,10 @@
+ //arrow function
+        function hello(name) {
+            console.log();
+        }   
+ // -->바꾸기
+        let hello = (name)=> {
+            console.log();
+        }
+ //변수값에 파라미터()가들어갈 수 없다
+
